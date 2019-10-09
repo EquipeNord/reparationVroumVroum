305cd8f57b54735b3c9fba69ee4ea764e694f240
JMainard <mainard17@outlook.fr>
2019-10-09 09:58:15 +0200

    ajout bdd

---
+++ ReparationsGarage_base.sql
@@ -0,0 +1,150 @@
+-- Base de données ReparationsGarage
+-- Générée avec GapMEA par Y. Salamone le 03/10/2019
+
+CREATE TABLE `Client`(
+	`cliNum` INTEGER,
+	`cliNom` VARCHAR(30),
+	`cliPrenom` VARCHAR(30),
+	`cliRue` VARCHAR(100),
+	`cliCP` VARCHAR(5),
+	`cliVille` VARCHAR(30),
+	`cliTel` VARCHAR(10),
+	`cliMobile` VARCHAR(10),
+	`cliMail` VARCHAR(50),
+	`cliLogin` VARCHAR(30),
+	`cliMDP` VARCHAR(255),
+	primary key(`cliNum`)
+)engine=innodb charset=utf8;
+
+CREATE TABLE `Facture`(
+	`factNum` INTEGER,
+	`factDate` DATE,
+	`factMontant_HT` FLOAT,
+	`factMontant_TTC` FLOAT,
+	`factPayee` BOOL,
+	primary key(`factNum`)
+)engine=innodb charset=utf8;
+
+CREATE TABLE `Utilisateur`(
+	`userNum` INTEGER,
+	`userNom` VARCHAR(30),
+	`userPrenom` VARCHAR(30),
+	`userLogin` VARCHAR(30),
+	`userMDP` VARCHAR(255),
+	primary key(`userNum`)
+)engine=innodb charset=utf8;
+
+CREATE TABLE `Role`(
+	`roleNum` INTEGER,
+	`roleLib` VARCHAR(25),
+	primary key(`roleNum`)
+)engine=innodb charset=utf8;
+
+CREATE TABLE `Piece`(
+	`pieceRef` VARCHAR(10),
+	`pieceLib` VARCHAR(100),
+	`pieceQteDispo` INTEGER,
+	`piecePU_HT` FLOAT,
+	`piecePU_TTC` FLOAT,
+	primary key(`pieceRef`)
+)engine=innodb charset=utf8;
+
+CREATE TABLE `Fournisseur`(
+	`fournisseurNum` INTEGER,
+	`fournisseurNom` VARCHAR(30),
+	`fournisseurTel` VARCHAR(10),
+	`fournisseurMail` VARCHAR(50),
+	primary key(`fournisseurNum`)
+)engine=innodb charset=utf8;
+
+CREATE TABLE `User_Role`(
+	`roleNum` INTEGER NOT NULL,
+	`userNum` INTEGER NOT NULL,
+   	foreign key (`roleNum`) references Role(`roleNum`), 
+	foreign key (`userNum`) references Utilisateur(`userNum`),
+	primary key(`roleNum`,`userNum`)
+)engine=innodb charset=utf8;
+
+CREATE TABLE `Commande`(
+	`commandeNum` INTEGER,
+	`dateLivraison` DATE,
+	`fournisseurNum` INTEGER NOT NULL,
+   	foreign key (`fournisseurNum`) references Fournisseur(`fournisseurNum`),
+	primary key(`commandeNum`)
+)engine=innodb charset=utf8;
+
+CREATE TABLE `Contenu`(
+	`quantite` INTEGER,
+	`commandeNum` INTEGER NOT NULL,
+	`pieceRef` VARCHAR(10) NOT NULL,
+   	foreign key (`commandeNum`) references Commande(`commandeNum`), 
+	foreign key (`pieceRef`) references Piece(`pieceRef`),
+	primary key(`commandeNum`,`pieceRef`)
+)engine=innodb charset=utf8;
+
+CREATE TABLE `Catalogue`(
+	`pieceRefFournisseur` VARCHAR(10),
+	`piecePUFournisseur` FLOAT,
+	`fournisseurNum` INTEGER NOT NULL,
+	`pieceRef` VARCHAR(10) NOT NULL,
+   	foreign key (`fournisseurNum`) references Fournisseur(`fournisseurNum`), 
+	foreign key (`pieceRef`) references Piece(`pieceRef`),
+	primary key(`fournisseurNum`,`pieceRef`)
+)engine=innodb charset=utf8;
+
+CREATE TABLE `Marque`(
+	`marqueNum` INTEGER,
+	`marqueNom` VARCHAR(30),
+	primary key(`marqueNum`)
+)engine=innodb charset=utf8;
+
+CREATE TABLE `Modele`(
+	`modeleNum` INTEGER,
+	`modeleNom` VARCHAR(30),
+	`marqueNum` INTEGER NOT NULL,
+   	foreign key (`marqueNum`) references Marque(`marqueNum`),
+	primary key(`modeleNum`)
+)engine=innodb charset=utf8;
+
+CREATE TABLE `Compatible`(
+	`modeleNum` INTEGER NOT NULL,
+	`pieceRef` VARCHAR(10) NOT NULL,
+   	foreign key (`modeleNum`) references Modele(`modeleNum`),
+   	foreign key (`pieceRef`) references Piece(`pieceRef`),
+	primary key(`modeleNum`,`pieceRef`)
+)engine=innodb charset=utf8;
+
+CREATE TABLE `Vehicule`(
+	`vehiculeNum` INTEGER,
+	`vehiculeAnnee` VARCHAR(4),
+	`vehiculeImmat` VARCHAR(8),
+	`modeleNum` INTEGER NOT NULL,
+   	foreign key (`modeleNum`) references Modele(`modeleNum`),
+	primary key(`vehiculeNum`)
+)engine=innodb charset=utf8;
+
+CREATE TABLE `Reparation`(
+	`reparNum` INTEGER,
+	`reparDateDebut` DATETIME,
+	`reparDateFin` DATETIME,
+	`reparDuree` INTEGER,
+	`userNum` INTEGER ,
+	`factNum` INTEGER ,
+	`cliNum` INTEGER NOT NULL,
+	`vehiculeNum` INTEGER NOT NULL,
+   	foreign key (`userNum`) references Utilisateur(`userNum`),
+   	foreign key (`factNum`) references Facture(`factNum`),
+   	foreign key (`cliNum`) references Client(`cliNum`),
+   	foreign key (`vehiculeNum`) references Vehicule(`vehiculeNum`),
+	primary key(`reparNum`)
+)engine=innodb charset=utf8;
+
+CREATE TABLE `Besoin`(
+	`quantite` INTEGER,
+	`reparNum` INTEGER NOT NULL,
+	`pieceRef` VARCHAR(10) NOT NULL,
+   	foreign key (`reparNum`) references Reparation(`reparNum`), 
+	foreign key (`pieceRef`) references Piece(`pieceRef`),
+	primary key(`reparNum`,`pieceRef`)
+)engine=innodb charset=utf8;
+
---
+++ ReparationsGarage_data.sql
@@ -0,0 +1,89 @@
+-- Jeu d'essai pour la base de données ReparationsGarage
+-- Créé le 03/10/2019 par Y. Salamone
+
+insert into Client values
+(1, 'Brochier', 'Anthony', '25 cours Émile Zola', '05000', 'Gap', '0492651235', '0651962417', 'brochieranthony.pro@gmail.com', 'antho_brc', password('pippoup')),
+(2, 'Salamone', 'Jodie', '28 boulevard Gassendi', '04000', 'Digne-les-Bains', '0492232845', '0640697140', 'jodie.slm@icloud.com', 'jodie.slm', password('mdpSecret1'));
+
+insert into Facture values
+(300, '2019-09-12', '100', '120', 0),
+(301, '2019-09-24', '200', '240', 1),
+(302, '2019-10-03', '300', '360', 0);
+
+insert into Utilisateur values
+(10, 'Salamone', 'Yohan', 'yohan.salamone', password('mdpFort1')),
+(11, 'Mainard', 'Jérémy', 'jeremylebgdu91', password('lebaratineur')),
+(12, 'Meozzi', 'Théo', 'thetmj', password('fortnite')),
+(13, 'Iron', 'Man', 'ironman', password('pasclovis'));
+
+insert into Role values
+(20, 'Gérant'),
+(21, 'Secrétaire'),
+(22, 'Comptable'),
+(23, 'Mécanicien'),
+(24, 'Magasinier');
+
+insert into User_Role values
+(20, 10),
+(23, 10),
+(22, 11),
+(21, 12);
+
+insert into Piece values
+('CDRN10', 'Courroie de distribution CONTITECH', 6, 25, 30),
+('FAPG3X', 'Filtre à air Bosch', 4, 15, 18);
+
+insert into Fournisseur values
+(40, 'SOPARTEX Gap', '0492536215', 'gap@sopartex.com'),
+(41, 'AUTODOC Gap', '0492567824', 'gap@autodoc.com');
+
+insert into Commande values
+(600, '2019-09-02', 40),
+(601, '2019-10-03', 41);
+
+insert into Contenu values
+(5, 600, 'CDRN10'),
+(2, 601, 'FAPG3X');
+
+insert into Catalogue values
+('SPGC48', 20, 40, 'CDRN10'),
+('SPGC61', 24, 41, 'CDRN10'),
+('AT9420', 14, 40, 'FAPG3X'),
+('AT5201', 12, 41, 'FAPG3X');
+
+insert into Marque values
+(80, 'Renault'),
+(81, 'Peugeot'),
+(82, 'Volvo'),
+(83, 'Volkswagen');
+
+insert into Modele values
+(800, 'Twingo 1', 80),
+(801, 'Clio 4', 80),
+(810, '208', 81),
+(811, '3008', 81),
+(820, 'V50', 82),
+(821, 'XC60', 82),
+(830, 'Polo', 83),
+(831, 'Touran', 83);
+
+insert into Compatible values
+(800, 'CDRN10'),
+(801, 'CDRN10'),
+(811, 'FAPG3X'),
+(821, 'FAPG3X'),
+(831, 'FAPG3X');
+
+insert into Vehicule values
+(1400, '2015', 'DP600JM', 801),
+(1401, '2012', 'CS133CW', 810),
+(1402, '2009', 'AE546HS', 821),
+(1403, '2000', 'DD294YF', 800);
+
+insert into Reparation values
+(12340, '2019-10-02 14:00:00', '2019-10-03 10:00:00', 6, 10, 302, 1, 1403),
+(12341, '2019-09-24 08:00:00', '2019-09-24 16:00:00', 6, 10, 301, 2, 1401);
+
+insert into Besoin values
+(1, 12340, 'CDRN10'),
+(1, 12341, 'FAPG3X');
