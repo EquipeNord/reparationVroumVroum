47203a7fe891460b56c130e3afe2d05a8893b214
antho-fly <brochieranthony.pro@gmail.com>
2019-10-10 15:41:55 +0200

    envoi de la mainwindow avec le dialog pour les taches du jour

--- reparationGarage/mainwindow.h
+++ reparationGarage/mainwindow.h
@@ -14,9 +14,26 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+    QString utilisateurId = "10";
+
+private slots:
+    void on_calendarWidgetPlanning_clicked(const QDate &date);
+
+    void on_pushButtonMecanicien_clicked();
+
+    void on_pushButtonSecretaire_clicked();
+
+    void on_pushButtonComptable_clicked();
+
+    void on_pushButtonMagasinier_clicked();
+
+    void on_pushButtonDirecteur_clicked();
+
+    void on_actionFermer_la_fen_tre_triggered();
 
 private:
     Ui::MainWindow *ui;
+    void setup();
 };
 
 #endif // MAINWINDOW_H
