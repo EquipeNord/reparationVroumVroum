4dd3d84dff23b373c61e6e2bd259d21caa9eab3f
Pandalas05 <granier.nicolas05@gmail.com>
2019-10-16 11:05:53 +0200

    Commit de mecno complet

--- reparationGarage/main.cpp
+++ reparationGarage/main.cpp
@@ -1,11 +1,34 @@
 #include "mainwindow.h"
+#include "dialogmecano.h"
 #include <QApplication>
+#include <QString>
+#include <QtSql/QSqlDatabase>
+#include <QDebug>
+
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+
+    QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
+    db.setHostName("localhost");
+    db.setDatabaseName("reparationVroumVroum");
+    db.setUserName("adminUserTaf");
+    db.setPassword("marmotte");
+    db.open();
+
+
+
+
+
+    DialogMecano test;
+    test.show();
+
+    //MainWindow w;
+    //w.show();
 
     return a.exec();
+
+
+
 }
--- reparationGarage/reparationGarage.pro
+++ reparationGarage/reparationGarage.pro
@@ -4,7 +4,7 @@
 #
 #-------------------------------------------------
 
-QT       += core gui
+QT       += core gui sql
 
 greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 
@@ -24,8 +24,14 @@ DEFINES += QT_DEPRECATED_WARNINGS
 
 
 SOURCES += main.cpp\
-        mainwindow.cpp
+        mainwindow.cpp \
+    dialogmecano.cpp
 
-HEADERS  += mainwindow.h
+HEADERS  += mainwindow.h \
+    dialogmecano.h
 
-FORMS    += mainwindow.ui
+FORMS    += mainwindow.ui \
+    dialogmecano.ui
+
+
+QT += sql
